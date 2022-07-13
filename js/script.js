191cb1ddcebbecc6c8667f68090d2a5e5fe62c25
bigeppo <carlog91cast@gmail.com>
2022-07-13 17:23:55 +0200

    add for to push mails to array

--- js/script.js
+++ js/script.js
@@ -14,14 +14,15 @@ const app = new Vue(
 
         },
         created() {
-            // mediante axios (ajax per vue) con un get domando le mail all'http 
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            // creando un arrow function per pusharli nell'array creato in data
-                .then((response) => {
-                    console.log(response);
-                    this.mails.push(response.data.response);
-                }
-                )
-        }
-
+            // creo un ciclo for per inserire le 10 mail nell'array
+            for (let i = 0; index < 10; i++) {
+                // mediante axios (ajax per vue) con un get domando le mail all'http 
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    // creando un arrow function per pusharli nell'array creato in data
+                    .then((response) => {
+                        this.mails.push(response.data.response);
+                        console.log(this.mails);
+                    });
+            };
+        },
     })
